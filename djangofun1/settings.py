b1f62256a6168bea61ee966161196e033b1868ac
Daisy Crego <daisycrego@gmail.com>
2019-10-02 15:27:11 -0400

    Changes based on advice from https://devcenter.heroku.com/articles/django-assets

--- djangofun1/settings.py
+++ djangofun1/settings.py
@@ -15,7 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #STATIC_ROOT = "/Users/porkchop/Library/Mobile Documents/com~apple~CloudDocs/ms/se1_python/djangoapp-dbc291/djangoapp-dbc291/static"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'polls/static')]
+
 #STATIC_ROOT = ''
 
 # https://stackoverflow.com/questions/23215581/unable-to-perform-collectstatic
@@ -56,6 +59,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djangofun1.urls'
@@ -126,10 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
 #STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'), os.path.join(BASE_DIR, 'polls/static')]
 
 
 
