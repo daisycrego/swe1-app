1dff1d1ac476b29c6404f499e9a08f764e5f381a
Daisy Crego <daisycrego@gmail.com>
2019-10-02 15:10:00 -0400

    More

--- djangofun1/settings.py
+++ djangofun1/settings.py
@@ -129,4 +129,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'), os.path.join(BASE_DIR, 'polls/static')]
+
+
+
+
