98d9ad2accb7ce40dddd2e2fa7b5c47ddb4f144b
Daisy Crego <daisycrego@gmail.com>
2019-10-02 13:08:22 -0400

    Minor change in settings.py. Trying to get my static files to show up...

--- djangofun1/settings.py
+++ djangofun1/settings.py
@@ -125,5 +125,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = 'polls/static/'
+STATIC_URL = '/static/'
 #STATIC_URL = '/static/'
