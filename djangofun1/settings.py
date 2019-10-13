eac4a1aeb4d600743435b581c954687ce044f58a
Daisy Crego <daisycrego@gmail.com>
2019-10-13 16:23:20 -0400

    Reformat files to pass both black and flake8 checks.

--- djangofun1/settings.py
+++ djangofun1/settings.py
@@ -101,7 +101,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+        "NAME": "django.contrib.auth.password_validation.\
+        UserAttributeSimilarityValidator"
     },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
