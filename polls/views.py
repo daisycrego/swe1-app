187a88f7dced8f85d869d899fc014ac0122e8808
Daisy Crego <daisycrego@gmail.com>
2019-10-14 14:39:06 -0400

    Move some of the installation steps to requirements.txt. Add coverage and python-coveralls to requirements.txt. Add command to invoke coverage instead of pytest. Add command to invoke coveralls after_success.

--- polls/views.py
+++ polls/views.py
@@ -12,9 +12,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """
-    	Return the last five published questions (not including those set to be
-    	published in the future).
-    	"""
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
             "-pub_date"
         )[:5]
